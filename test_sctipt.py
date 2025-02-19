78e27c9e0d4d7d94a78f1f1778e62c7f87895ddc
monnortarts <127376114+monnortarts@users.noreply.github.com>
2025-02-19 09:50:32 +0800

    Add files via upload

--- test_sctipt.py
+++ test_sctipt.py
@@ -1,2 +1,6 @@
-print("testiboy")
-input("Press Enter To Exit")
+import updater_interfacer
+
+updater_interfacer.version = 0
+
+input(f"Newest Version: {updater_interfacer.get_latest_version()}\nCurrent Version: {updater_interfacer.version}\nPress Enter to update.")
+updater_interfacer.update()
