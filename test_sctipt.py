8aedbb858e42505cc50635aa92a2a4478280ceee
monnortarts <127376114+monnortarts@users.noreply.github.com>
2025-02-19 11:50:03 +0800

    Update test_sctipt.py

--- test_sctipt.py
+++ test_sctipt.py
@@ -1,6 +1,6 @@
 import updater_interfacer
 
-updater_interfacer.version = 0
+updater_interfacer.version = float(1.0.0)
 
 input(f"Newest Version: {updater_interfacer.get_latest_version()}\nCurrent Version: {updater_interfacer.version}\nPress Enter to update.")
 updater_interfacer.update()
