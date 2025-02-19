34862e50e580fb0786f72f1ffd1fc33602c6c0c7
monnortarts <127376114+monnortarts@users.noreply.github.com>
2025-02-19 13:17:57 +0800

    Update test_sctipt.py

--- test_sctipt.py
+++ test_sctipt.py
@@ -1,6 +1,6 @@
 import updater_interfacer
 
-updater_interfacer.version = '2.0.0'
+updater_interfacer.version = '3.0.0'
 
 input(f"Newest Version: {updater_interfacer.get_latest_version()}\nCurrent Version: {updater_interfacer.version}\nPress Enter to update.")
 updater_interfacer.update()
